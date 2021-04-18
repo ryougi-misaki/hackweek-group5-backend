ee389b07b3c7d83d27302d6e5995380a00cb1119
lxy <1500868816@qq.com>
2021-04-18 08:10:26 +0800

    鍚堝苟鍒嗘敮

--- hackathon/routes/routes.go
+++ hackathon/routes/routes.go
@@ -23,6 +23,7 @@ func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("api/auth/register", controller.Register)
 	r.POST("api/auth/login", controller.Login)
 	r.GET("api/auth/info", middleware.AuthMiddleware(), controller.Info)
+
 	r.GET("/ws/:id" , controller.WebSocket)
 	r.POST("/chat/history",controller.ChatHistory)
 	return r
